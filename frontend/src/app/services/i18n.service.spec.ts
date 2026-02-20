75bb0ea004074ef6378df4fd85995d081a12933f
aitema Deploy <deploy@aitema.de>
2026-02-20 14:48:56 +0000

    test: add I18nService unit test spec

---
+++ frontend/src/app/services/i18n.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { I18nService } from './i18n.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('I18nService', () => {
+  let service: I18nService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(I18nService);
+  });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+  it('should default to German', () => {
+    expect(service.getCurrentLocale()).toBe('de');
+  });
+});
